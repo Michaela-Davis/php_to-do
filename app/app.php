f3fbf4ac08e1f985c76d6aee51d4fdfc5bc95747
Carlos Munoz Kampff and Michaela Davis <michaela@>
2017-02-08 09:56:51 -0800

    init commit

---
+++ app/app.php
@@ -0,0 +1,43 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Task.php";
+
+    session_start();
+    if (empty($_SESSION['list_of_tasks'])) {
+        $_SESSION['list_of_tasks'] = array();
+    }
+
+    $app = new Silex\Application();
+
+    $app->get("/", function() {
+
+        $output = "";
+
+        foreach (Task::getAll() as $task) {
+            $output = $output . "<p>" . $task->getDescription() . "</p>";
+        }
+
+        $output = $output . "
+            <form action='/tasks' method='post'>
+                <label for='description'>Task Description</label>
+                <input id='description' name='description' type='text'>
+
+                <button type='submit'>Add task</button>
+            </form>
+        ";
+
+        return $output;
+    });
+
+    $app->post("/tasks", function() {
+        $task = new Task($_POST['description']);
+        $task->save();
+        return "
+            <h1>You created a task!</h1>
+            <p>" . $task->getDescription() . "<p>
+            <p><a href='/'>View your list of things to do.</a></p>
+        ";
+    });
+    return $app;
+?>
